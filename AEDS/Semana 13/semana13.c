097ffbbe441cc81025fd2e80ccf0704bf1efbdbb
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2022-12-03 17:10:01 -0300

    adicionado semana13.c

---
+++ AEDS/Semana 13/semana13.c
@@ -0,0 +1,27 @@
+/*Na semana 11, utilizamos o exercício abaixo para treinar o uso
+dos algoritmos de ordenação Seleção e Inserção:
+Carlos adora jogar Pif, um jogo de cartas onde o objetivo é juntar três
+trios na mão do jogador, sendo que cada trio pode conter 3 cartas em
+sequência, ou três cartas com o mesmo número. Porém, Carlos tem um
+péssimo hábito de não organizar suas cartas, e muitas vezes acaba
+descartando cartas úteis por isto. Seu objetivo é aplicar o algoritmo de
+ordenação simples indicado para ordenar a sequência de cartas de
+cada letra abaixo, verificando se Carlos conseguiu montar trincas.
+Observe que:
+● Cada carta é representada da seguinte forma: XN, sendo que X é
+o número da carta, e N é o naipe, podendo ser: (P) Paus, (O)
+Ouros, (C) Copas e (E) Espadas.
+● O valete corresponde ao J, a dama ao Q, e o rei corresponde ao K
+Na semana 12 ordenamos uma sequência de cartas para o
+jogo utilizando os algoritmos de inserção e shellsort.
+Agora, utilizando essa mesma sequência, vamos ordená-la
+utilizando o algoritmo Quicksort.
+KE - QE - 8C - JE - 8E - 1O - 2O - 8O - 3O - 6E - 7O - 1P - 4C -
+10E
+Ao final, compare o uso dos algoritmos da semana 12, em
+termos do número de comparações e o número de
+movimentações necessárias, com o algoritmo Quicksort. Faça
+a ordenação manualmente utilizando o passo a passo da
+implementação para o algoritmo visto em sala de aula. Anote
+todas as comparações e movimentações, bem como o estado
+de cada partição ao final de seu processamento.*/
